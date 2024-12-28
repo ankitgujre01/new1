497bafa97f4499115d5a863fbf6cf549aae11a84
ankitgujre <kumarankit65494@gmail.com>
2024-12-28 17:44:24 +0530

    new changes

--- demo_project/demo_project/urls.py
+++ demo_project/demo_project/urls.py
@@ -19,6 +19,7 @@ from django.urls import path
 from myApp.views import home
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home')
+    path('', home, name='home'),
+    path('')
 ]
 
--- demo_project/myApp/views.py
+++ demo_project/myApp/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
 
 def home(request):
     # return HttpResponse("<h1 style = 'background-color:grey'>Hellooo......</h1>")
-    return render(request, 'index.html')
+    # return render(request, 'index.html')
+    
+    # data = {"name":"amit", "age":"22", "active":"true", "active1":"null"}   #JSON Data
+    # return JsonResponse(data)
+    
+    return redirect('https://www.djangoproject.com/start/')
